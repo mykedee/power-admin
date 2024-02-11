0bd699c9c7fea614b15f422b48232098b7bc665a
lakes <mikededeji@gmail.com>
2024-02-11 20:35:09 +0100

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,8 @@ app.use("/api/v1", postRoutes);
 if (process.env.NODE_ENV === "production") {
 	const __dirname = path.resolve();
 	app.use("/uploads", express.static("/var/data/uploads"));
-	app.use(express.static(path.join(__dirname, "uploads")));
-	// app.use(express.static(path.join(__dirname, "/frontend/build")));
+	app.use(express.static(path.join(__dirname, "/uploads")));
+	app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 	app.get("*", (req, res) =>
 		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
