ec56e336fec1e8c472e76f981a92e3774a17bd07
lakes <mikededeji@gmail.com>
2024-02-22 21:52:27 +0100

    recent commit

--- frontend/src/components/Common/NotificationCard.js
+++ frontend/src/components/Common/NotificationCard.js
@@ -6,7 +6,7 @@ import { GrClose } from "react-icons/gr";
 const NotificationCard = ({ setShowNotificationCard }) => {
 	return (
 		<div className="relative py-5">
-			<div className="bg-white dark:bg-slate-700 h-68 w-[98vw] md:w-80 absolute top-16 md:top-10 -right-20 md:right-6 rounded shadow">
+			<div className="bg-white dark:bg-slate-700 h-68 w-[98vw] md:w-80 absolute top-16 md:top-10 -right-16 md:right-6 rounded shadow">
 				<div className="flex justify-between items-center p-5 border-b">
 					<p className="mx-3">Notifications</p>
 					<button className="right-0 bg-white text-white dark:bg-primary-orange shadow rounded-full p-2 hover:shadow-md">
