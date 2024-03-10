34f03d284735fe721c2926429a0d0370afb28808
mykedee <52767074+mykedee@users.noreply.github.com>
2024-03-10 11:31:42 +0100

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -6,6 +6,7 @@ const sendEmail = async (options) => {
  const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
+  service:gmail,
   secure: true, 
   auth: {
    user: process.env.SMTP_USERNAME, // generated ethereal user
