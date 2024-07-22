6822a8ae355918d41d326f4abaae6268209bf1a0
mykedee <mikededeji@gmail.com>
2024-07-22 08:27:27 +0100

    recent update

--- frontend/src/components/Common/CardContainer.js
+++ frontend/src/components/Common/CardContainer.js
@@ -3,7 +3,7 @@ import Footer from "../Footer/Footer";
 
 const CardContainer = ({ children }) => {
 	return (
-		<div className="min-h-full rounded my-4 w-full lg:ml-[20vw] mx-auto">
+		<div className="min-h-full rounded my-4 w-full lg:ml-[15vw] mx-auto">
 			<div className="w-11/12 md:w-10/12 lg:w-10/12 mx-auto">{children}</div>
 			<Footer/>
 		</div>
